1f44ea4b04fb035d773da183e0c664ece16ea3fc
Michal Zagrodzki <michal@husleie.no>
2018-05-09 09:48:41 +0200

    Add cors to project

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors')
+const Ch = require('cheerio');
 
 const app = express();
 
 app.use('/', express.static(path.join(__dirname + '/dist')));
 
-app.get('/', (req, res) => {
+app.get('/', cors(), (req, res) => {
   res.send(console.log('message from server'));
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.send(req)
+  res.send(req);
 });
 
 console.log('Serving app at 7080')
