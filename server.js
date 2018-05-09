acb7d4eb7227c651043e4c0aa297d634dc920745
Michal Zagrodzki <michal@husleie.no>
2018-05-09 18:18:58 +0200

    Move cross-origin options to app.use function. Add missing options.

--- server.js
+++ server.js
@@ -7,10 +7,16 @@ const app = express();
 
 app.use('/', express.static(path.join(__dirname + '/dist')));
 
+app.use(function(req, res, next){
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+})
+
 app.get('/', cors(), (req, res) => {
   res.send(console.log('message from server'));
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.send(req);
+  res.send(res);
 });
 
 console.log('Serving app at 7080')
