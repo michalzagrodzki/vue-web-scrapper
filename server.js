ae26f8db9a7d52f31af9cb9b1babeb16eb42262c
Michal Zagrodzki <michal@husleie.no>
2018-06-02 11:48:33 +0200

    Add conditional for elements within list of authors

--- server.js
+++ server.js
@@ -32,11 +32,14 @@ app.get('/api', (req, res, next) =>{
       Ch('script').remove()
       Ch('div #toc').remove()
       Ch('li').each(function(i, elem){
-        result[i] = {
+        if(Ch(this).parent().parent().prev().children().attr('id')) {
+          result[i] = {
           name: Ch(this).has('a').text(),
           letter: Ch(this).parent().parent().prev().children().attr('id'),
           link: 'https://en.wikipedia.org' + Ch(this).children('a').attr('href')
            } 
+        }
+        
       })
       res.send(result)
     }
